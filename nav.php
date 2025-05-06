81054802633dc58b7a15c28cf7983223c1ff0a51
Kurt <trythisnet03@gmail.com>
2025-05-06 20:59:40 +0800

    -added session check and nav bar

---
+++ nav.php
@@ -0,0 +1,11 @@
+<nav class="navbar">
+    <div class="header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse">
+            <span class="sr-only"> Toggle Navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a href="admin.php" class="navbar-brand">PHP CRUD using MySQL</a>
+    </div>
+</nav>
